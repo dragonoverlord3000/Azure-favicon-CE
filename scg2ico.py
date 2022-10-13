b6ccbca337a86064783990964b5cf3bae3a53833
Your Name <hugomn2002@gmail.com>
2022-10-13 12:27:33 +0200

    It's working

--- scg2ico.py
+++ scg2ico.py
@@ -3,28 +3,30 @@ from PIL import Image
 from io import BytesIO
 import cairosvg
 
+import base64
+
 root = "./icons (1)/"
 
 # Write name_dict to s_list.txt
-l = [f"\"{file[:-4].lower().replace(' ', '')}\": \"{file[:-4]}.ico\"," for file in os.listdir("./icons (1)")]
+l = [f"\"{file[:-4].lower().replace(' ', '')}\": \"https://azure-favicons-bucket.s3.amazonaws.com/icons+(1)/{file[:-4].replace(' ', '+')}.ico\"," for file in os.listdir("./icons (1)") if not file[:4] == "FxSy"]
 with open("./s_list.txt", "w") as f: 
     f.write("\n".join(l))
 
 
-# Convert SVG's to ICO's
-print(f"Length before: {len(os.listdir(root))}")
+## Convert SVG's to ICO's
+# print(f"Length before: {len(os.listdir(root))}")
 
-failed = []
-for im_name in os.listdir(root):
-    try: 
-        out = BytesIO()
-        cairosvg.svg2png(url=f"{root}{im_name}", write_to=out)
-        image = Image.open(out)
-        icon_sizes = [(16,16), (32, 32), (48, 48), (64,64)]
-        image.save(f"{root}{im_name[:-4]}.ico", sizes=icon_sizes)
-    except:
-        failed.append(im_name)
-        print(im_name)
+# failed = []
+# for im_name in os.listdir(root):
+#     try: 
+#         out = BytesIO()
+#         cairosvg.svg2png(url=f"{root}{im_name}", write_to=out)
+#         image = Image.open(out)
+#         icon_sizes = [(16,16), (32, 32), (48, 48), (64,64)]
+#         image.save(f"{root}{im_name[:-4]}.ico", sizes=icon_sizes)
+#     except:
+#         failed.append(im_name)
+#         print(im_name)
 
-print(f"Length after: {len(os.listdir(root))}")
-print(f"# failed = {len(failed)}")
+# print(f"Length after: {len(os.listdir(root))}")
+# print(f"# failed = {len(failed)}")
